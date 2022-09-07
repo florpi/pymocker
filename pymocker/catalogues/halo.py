2be680fd5e1c777e8ca63742ec0fc5310a1b20d7
Carolina Cuesta <florpisoteadadejardin@gmail.com>
2022-09-07 14:43:52 -0400

    added docstring

--- pymocker/catalogues/halo.py
+++ pymocker/catalogues/halo.py
@@ -2,6 +2,7 @@ from pymocker.catalogues.base import Catalogue
 import numpy as np
 from typing import Optional
 
+
 class HaloCatalogue(Catalogue):
     def __init__(
         self,
@@ -11,6 +12,15 @@ class HaloCatalogue(Catalogue):
         boxsize: Optional[float] = None,
         redshift: Optional[float] = None,
     ):
+        """Catalogue of dark matter halos
+
+        Args:
+            pos (np.array): array of positions of size (N, 3)
+            vel (np.array): array of velocities of size (N, 3)
+            mass (np.array): array of halo masses
+            boxsize (Optional[float], optional): size of the simulated box. Defaults to None.
+            redshift (Optional[float], optional): redshift of simulated snapshot. Defaults to None.
+        """
         self.pos = pos
         self.vel = vel
         self.mass = mass
